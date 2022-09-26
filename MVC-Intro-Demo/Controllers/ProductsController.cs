6521b32cd408287f2877c02ecea1547749dfc5f3
TOKA-PC <TOKA-PC@DESKTOP-9F5CI74>
2022-09-26 10:27:30 +0300

    Add project files.

---
+++ MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Text.Json;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.5
+            },
+
+            new ProductViewModel()
+            { 
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
+        {
+            if (keyword != null)
+            {
+                var foundProducts = this.products.Where(pr => pr.Name.ToLower().Contains(keyword.ToLower()));
+                
+                return View(foundProducts);
+            }
+            return View(this.products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = this.products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            return Json(products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            var text = String.Empty;
+            foreach (var product in products)
+            {
+                text += $"Product {product.Id}: {product.Name} - {product.Price} lv.";
+                text += "\r\n";
+            }
+            return Content(text);
+        }
+
+
+    }
+}
